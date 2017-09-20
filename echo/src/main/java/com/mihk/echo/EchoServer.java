31fed3d9f06d6cb88bfd0cee568d50c0851d9934
Jackson Mi <1045262335@qq.com>
2017-09-20 11:44:45 +0800

    server end

---
+++ echo/src/main/java/com/mihk/echo/EchoServer.java
@@ -0,0 +1,64 @@
+package com.mihk.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import javax.net.ssl.SSLException;
+import java.security.cert.CertificateException;
+
+public class EchoServer {
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+
+    public static void main(String[] args) throws CertificateException, SSLException {
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+
+        } else {
+            sslCtx = null;
+        }
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                                      @Override
+                                      public void initChannel(SocketChannel ch) throws Exception {
+                                          ChannelPipeline pipeline = ch.pipeline();
+                                          if (sslCtx != null) {
+                                              pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+                                          }
+                                          pipeline.addLast((ChannelHandler) new EchoServerHandler());
+                                      }
+                                  }
+                    );
+
+            ChannelFuture future = null;
+
+            future = bootstrap.bind(PORT).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+
+    }
+
+}
